943ae69f3114fb5a5324eed2d43ae6daaceae8fa
Shun Fukusumi <fukusumi.shun@classmethod.jp>
2022-06-25 20:00:11 +0900

    read s3 object

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 import argparse
+import boto3
+from botocore.config import Config
+
+boto3_config = Config(
+    connect_timeout=10, retries={"max_attempts": 3, "mode": "standard"}
+)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--bucket')
@@ -7,5 +13,6 @@ parser.add_argument('--object')
 if __name__ == "__main__":
     args = parser.parse_args()
 
-    print(args.bucket)
-    print(args.object)
+    s3 = boto3.client("s3", config=boto3_config)
+    res = s3.get_object(Bucket=args.bucket, Key=args.object)
+    print(res["Body"].read().decode("utf-8"))
