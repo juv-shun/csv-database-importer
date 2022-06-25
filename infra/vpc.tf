1e00719669c3dbeeb842208f518f57840aa941d0
Shun Fukusumi <fukusumi.shun@classmethod.jp>
2022-06-25 22:27:57 +0900

    VPC created

---
+++ infra/vpc.tf
@@ -0,0 +1,130 @@
+#####################################
+# VPC Settings
+#####################################
+resource "aws_vpc" "vpc" {
+  cidr_block           = "172.16.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = var.service_name
+  }
+}
+
+#####################################
+# Public Subnets Settings
+#####################################
+resource "aws_subnet" "public_subnet_az1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "172.16.1.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "[${var.service_name}] public_subnet_az1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_az2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "172.16.2.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "[${var.service_name}] public_subnet_az2"
+  }
+}
+
+#####################################
+# Private Subnets Settings
+#####################################
+resource "aws_subnet" "private_subnet_az1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "172.16.3.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "[${var.service_name}] private_subnet_az1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_az2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "172.16.4.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "[${var.service_name}] private_subnet_az2"
+  }
+}
+
+#####################################
+# Internet Gateway Settings
+#####################################
+resource "aws_internet_gateway" "i_gw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = var.service_name
+  }
+}
+
+#####################################
+# Routes Table Settings
+#####################################
+resource "aws_route_table" "igw_route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.i_gw.id
+  }
+  tags = {
+    Name = "${var.service_name}_igw"
+  }
+}
+
+#####################################
+# Routes Association Settings
+#####################################
+resource "aws_route_table_association" "i_gw_association_az1" {
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.igw_route.id
+}
+
+resource "aws_route_table_association" "i_gw_association_az2" {
+  subnet_id      = aws_subnet.public_subnet_az2.id
+  route_table_id = aws_route_table.igw_route.id
+}
+
+
+#####################################
+# Security Group
+#####################################
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["172.16.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "[${var.service_name}] default"
+  }
+}
